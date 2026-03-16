931e7ca7078669d706b83b6395e12de398de7928
yampug <bpsekii@gmail.com>
2026-03-16 19:49:08 +0100

    context-aware ast autocomplete

---
+++ languages/crystal/folds.scm
@@ -0,0 +1,30 @@
+[
+  (annotation_def)
+  (begin)
+  (block)
+  (c_struct_def)
+  (case)
+  (class_def)
+  (else)
+  (enum_def)
+  (fun_def)
+  (if)
+  (lib_def)
+  (method_def)
+  (module_def)
+  (rescue)
+  (select)
+  (struct_def)
+  (union_def)
+  (when)
+  (while)
+] @fold
+
+[
+  (array)
+  (array_like)
+  (hash)
+  (hash_like)
+  (named_tuple)
+  (tuple)
+] @fold
---
+++ languages/crystal/injections.scm
@@ -0,0 +1,50 @@
+((comment) @injection.content
+  (#set! injection.language "comment"))
+
+(regex
+  (literal_content) @injection.content
+  (#set! injection.language "regex"))
+
+(heredoc_body
+  (literal_content) @injection.content
+  (heredoc_end) @injection.language)
+
+(macro_def
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
+
+(macro_begin
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
+
+(macro_if
+  then: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
+
+(macro_elsif
+  then: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
+
+(macro_else
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
+
+(macro_unless
+  then: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
+
+(macro_for
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
+
+(macro_verbatim
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal")
+  (#set! injection.include-children))
