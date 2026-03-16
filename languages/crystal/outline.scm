e1f0e3920227c1e72abd5616651fa143af501354
yampug <bpsekii@gmail.com>
2026-03-16 19:37:16 +0100

    code struc and indentation

---
+++ languages/crystal/outline.scm
@@ -0,0 +1,20 @@
+(class_def
+  name: (_) @name) @item
+
+(module_def
+  name: (_) @name) @item
+
+(struct_def
+  name: (_) @name) @item
+
+(enum_def
+  name: (_) @name) @item
+
+(lib_def
+  name: (_) @name) @item
+
+(method_def
+  name: (_) @name) @item
+
+(macro_def
+  name: (_) @name) @item
